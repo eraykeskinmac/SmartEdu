887b787c2e4948216e8afba7a34e5db32a77c6e0
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-06-04 22:51:31 +0300

    views and public files created and edited

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('İNDEX SAYFASI');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
 });
 
 const port = 3000;
