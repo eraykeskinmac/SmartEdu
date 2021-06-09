ec5374bbe8b0c7df5f958e578da02a559b97b34e
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-06-10 01:19:42 +0300

    user roles created

--- models/User.js
+++ models/User.js
@@ -17,6 +17,11 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student',
+  },
 });
 
 UserSchema.pre('save', function (next) {
