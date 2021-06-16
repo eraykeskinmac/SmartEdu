070b5d5d42cffc807bd14cf57b0ab142815a0137
Mustafa Eray Keskin <meraykeskin@gmail.com>
2021-06-16 15:45:31 +0300

    updated course

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -10,6 +10,7 @@ router
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').put(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.relaseCourse);
 
